dae3df8b7c6c70f47bf046df5cf27f8bf09e9de9
Muchammad Indra <muchammadindra97@gmail.com>
2023-10-07 17:12:55 +0700

    feat<app>: setup mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
+import '@mantine/core/styles.css';
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../../theme";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <title>SIMPUS Dev Helper</title>
+        <meta name="description" content="Helper untuk development aplikasi SIMPUS" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
