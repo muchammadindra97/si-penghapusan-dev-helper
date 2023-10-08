b80155c8ea6b5bd6d030c3178e776355d1aa1514
Muchammad Indra <muchammadindra97@gmail.com>
2023-10-08 18:56:32 +0700

    update<user-creator>: service add master roles

--- src/services/userCreatorService.ts
+++ src/services/userCreatorService.ts
@@ -1,3 +1,47 @@
+export type MASTER_ROLE = {
+	name: string,
+	level: '2'|'1'|'0'|'multi'
+}
+
+export const MASTER_ROLES = new Map<string, MASTER_ROLE>([
+	['creator-lv2', { name: 'creator-lv2', level: '2'}],
+	['reviewer-lv2', { name: 'reviewer-lv2', level: '2'}],
+	['approver-lv2', { name: 'approver-lv2', level: '2'}],
+	['creator-lv1', { name: 'creator-lv1', level: '1'}],
+	['reviewer-lv1', { name: 'reviewer-lv1', level: '1'}],
+	['approver-lv1', { name: 'approver-lv1', level: '1'}],
+	['creator-spi', { name: 'creator-spi', level: 'multi'}],
+	['reviewer-spi', { name: 'reviewer-spi', level: 'multi'}],
+	['approver-spi', { name: 'approver-spi', level: 'multi'}],
+	['creator-hukum', { name: 'creator-hukum', level: 'multi'}],
+	['reviewer-1-hukum', { name: 'reviewer-1-hukum', level: 'multi'}],
+	['reviewer-2-hukum', { name: 'reviewer-2-hukum', level: 'multi'}],
+	['approver-hukum', { name: 'approver-hukum', level: 'multi'}],
+	['creator-resiko', { name: 'creator-resiko', level: 'multi'}],
+	['reviewer-1-resiko', { name: 'reviewer-1-resiko', level: 'multi'}],
+	['reviewer-2-resiko', { name: 'reviewer-2-resiko', level: 'multi'}],
+	['approver-resiko', { name: 'approver-resiko', level: 'multi'}],
+	['creator-pembina', { name: 'creator-pembina', level: 'multi'}],
+	['reviewer-1-pembina', { name: 'reviewer-1-pembina', level: 'multi'}],
+	['reviewer-2-pembina', { name: 'reviewer-2-pembina', level: 'multi'}],
+	['approver-pembina', { name: 'approver-pembina', level: 'multi'}],
+	['creator-kepatuhan', { name: 'creator-kepatuhan', level: 'multi'}],
+	['reviewer-1-kepatuhan', { name: 'reviewer-1-kepatuhan', level: 'multi'}],
+	['reviewer-2-kepatuhan', { name: 'reviewer-2-kepatuhan', level: 'multi'}],
+	['approver-kepatuhan', { name: 'approver-kepatuhan', level: 'multi'}],
+	['creator-setper', { name: 'creator-setper', level: 'multi'}],
+	['reviewer-1-setper', { name: 'reviewer-1-setper', level: 'multi'}],
+	['reviewer-2-setper', { name: 'reviewer-2-setper', level: 'multi'}],
+	['approver-setper', { name: 'approver-setper', level: 'multi'}],
+	['creator-pusat', { name: 'creator-pusat', level: 'multi'}],
+	['reviewer-1-pusat', { name: 'reviewer-1-pusat', level: 'multi'}],
+	['reviewer-2-pusat', { name: 'reviewer-2-pusat', level: 'multi'}],
+	['approver-pusat', { name: 'approver-pusat', level: 'multi'}],
+	['creator-tp2dp', { name: 'creator-tp2dp', level: 'multi'}],
+	['reviewer-tp2dp', { name: 'reviewer-tp2dp', level: 'multi'}],
+	['approver-tp2dp', { name: 'approver-tp2dp', level: 'multi'}]
+])
+
 export function generateUserName(roleName: string, bussArea: string): string {
 	return `${roleName.replaceAll('-', '_')}_${bussArea}`
 }
@@ -13,7 +57,7 @@ export function generateQueryInsertUser(roleName: string, bussArea: string, isWi
 	return `INSERT INTO "USERS" (${isWithDbId ? '"ID", ' : ''}"USERNAME", "NAME", "EMAIL", "password", "ACTIVE_DIRECTORY", "STATUS", "CREATED_AT") VALUES (${isWithDbId ? `(SELECT NVL(MAX(ID), 0)+1 FROM "USERS"), ` : ''}'${userName}', '${name}', '${userName}@email.test', '$2y$10$9PCJ.5bX0RHt1Zq7gFiswOcigO1kvwXRP5V.xgG3rpSVbYY4S4Pvm', '0', 'ACTV', SYSTIMESTAMP);`
 }
 
-export function generateQueryAssignRole(roleName: string, bussArea: string, isWithDbId: boolean = false): string {
+export function generateQueryAssignRole(roleName: string, bussArea: string): string {
 	const userName = generateUserName(roleName, bussArea)
 	const name = `${roleName.replaceAll('-', ' ')} ${bussArea}`.toUpperCase()
 
